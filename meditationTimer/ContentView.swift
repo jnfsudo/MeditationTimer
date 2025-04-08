5c190e7a353d29ec83f20144c24143c03c232732
Jeremy Fischer <jnfischer@icloud.com>
2025-04-08 16:28:07 -0400

    Increased time to 10min, formatted time to look like minutes and seconds, resets to 10min

--- meditationTimer/ContentView.swift
+++ meditationTimer/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @State private var timeRemaining = 60 //set initial timer duration
+    @State private var timeRemaining = 600 //set initial timer duration
     @State private var timerRunning = false
     @State private var timer: Timer?
     @Environment(\.modelContext) private var modelContext
@@ -18,7 +18,7 @@ struct ContentView: View {
     var body: some View {
         VStack{
             Text("Meditation Timer")
-            Text("\(timeRemaining)")
+            Text(timeFormatted(timeRemaining))
                 .font(.largeTitle)
                 .padding()
             HStack {
@@ -55,9 +55,13 @@ struct ContentView: View {
     }
     func resetTimer(){
         stopTimer()
-        timeRemaining = 60 //reset to initial value
+        timeRemaining = 600 //reset to initial value
     }
-    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let minutes = totalSeconds / 60
+        let seconds = totalSeconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
     
     /*    private func addItem() {
      withAnimation {
